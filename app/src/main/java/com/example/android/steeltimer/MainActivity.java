0532e80a8249421b51c12977c3b77b33181272e7
Jake <jake024d@gmail.com>
2018-03-13 04:37:00 -0400

    Clean everything up, added in notes throughout the code.

--- app/src/main/java/com/example/android/steeltimer/MainActivity.java
+++ app/src/main/java/com/example/android/steeltimer/MainActivity.java
@@ -7,9 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Calendar;
+
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    //Base values of all three timers
     private static final long START_TIME_IN_MILLIS = 201600000;
     private static final long BURN_START_IN_MILLIS = 43200000;
     private static final long COAL_START_IN_MILLIS = 4800000;
@@ -19,8 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextViewCountDown;
     private TextView mTextViewCountDownTwo;
     private TextView mTextViewCountDownThree;
-    private Button mButtonStartPause;
-    private Button mButtonReset;
+
+    private Button mButtonStartReset;
     private Button mButtonAddCoal;
 
     private CountDownTimer mCountDownTimer;
@@ -28,56 +31,69 @@ public class MainActivity extends AppCompatActivity {
     private CountDownTimer mCurrentCoalTimer;
 
     private boolean mTimerRunning;
-    private boolean mCurrentBurnRunning;
-    private boolean mCurrentCoalRunning;
 
     private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
     private long mCurrentBurnInMillis = BURN_START_IN_MILLIS;
     private long mCurrentCoalInMillis = COAL_START_IN_MILLIS;
+
+    //values for onSaveInstanceState
+    private Calendar calender = Calendar.getInstance();
+
+    private long startTime = 0;
+    private long finishTime = 0;
+    private long currentFinishTime = 0;
+    private long coalFinishTime = 0;
+
+    private String FINISH_TIME = "whenFullTimerRunsOutInMillis";
+    private String FINAL_TIMER_RUNNING = "booleanValue";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // recovering the instance state
+        if (savedInstanceState != null) {
+            super.onCreate(savedInstanceState);
+            mTimerRunning = savedInstanceState.getBoolean(FINAL_TIMER_RUNNING);
+            if (mTimerRunning) {
+                finishTime = savedInstanceState.getLong(FINISH_TIME);
+                startTimer();
+            }
+        }
+
+        //View Id Assignments
         mTextViewCountDown = findViewById(R.id.text_view_countdown);
         mTextViewCountDownTwo = findViewById(R.id.text_view_countdown_two);
         mTextViewCountDownThree = findViewById(R.id.text_view_countdown_three);
 
-        mButtonStartPause = findViewById(R.id.button_start_pause);
-        mButtonReset = findViewById(R.id.button_reset);
-
+        mButtonStartReset = findViewById(R.id.button_start_pause);
         mButtonAddCoal = findViewById(R.id.button_add_coal);
 
-        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+        //Initializing Start Button Listener
+        mButtonStartReset.setOnClickListener(new View.OnClickListener() {
+
+            /**
+             * onClick method that handles the startReset button.
+             * @param v startReset Button
+             */
             @Override
             public void onClick(View v) {
                 if (mTimerRunning) {
-                    pauseTimer();
+                    resetTimer();
                 } else {
+                    startTime = calender.getTimeInMillis();
+                    finishTime = startTime + START_TIME_IN_MILLIS;
+                    currentFinishTime = startTime + BURN_START_IN_MILLIS;
+                    coalFinishTime = startTime + COAL_START_IN_MILLIS;
                     startTimer();
+                }
 
-                }
-                if (mCurrentBurnRunning) {
-                    pauseBurnTimer();
-                } else {
-                    startCurrentBurnTimer();
-                }
-                if (mCurrentCoalRunning) {
-                    pauseCoalTimer();
-                } else {
-                    startCurrentCoalTimer();
-                }
             }
+
         });
 
-        mButtonReset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                resetTimer();
-            }
-        });
-
+//         Method that adds coal to the fuel load
         mButtonAddCoal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,7 +104,13 @@ public class MainActivity extends AppCompatActivity {
         updateCountDownText();
     }
 
+    /**
+     * Method that handles the timers.
+     */
     private void startTimer() {
+
+//         Starting the final countdown timer
+        mTimeLeftInMillis = finishTime - calender.getTimeInMillis();
         mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -99,19 +121,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
                 mTimerRunning = false;
-                mButtonStartPause.setText("Start");
-                mButtonStartPause.setVisibility(View.INVISIBLE);
-                mButtonReset.setVisibility(View.VISIBLE);
+                mButtonStartReset.setText("Start");
             }
         }.start();
 
         mTimerRunning = true;
-        mButtonStartPause.setText("pause");
-        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartReset.setText("Reset");
         mButtonAddCoal.setVisibility(View.VISIBLE);
-    }
 
-    private void startCurrentBurnTimer() {
+//         Starting the current burn countdown timer
+        mTimeLeftInMillis = currentFinishTime - calender.getTimeInMillis();
         mCurrentBurnTimer = new CountDownTimer(mCurrentBurnInMillis, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -122,13 +141,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
                 resetTimer();
+                String RUN_OUT_TEXT = "Forge went out :(";
+                mTextViewCountDownTwo.setText(RUN_OUT_TEXT);
             }
         }.start();
 
-        mCurrentBurnRunning = true;
-    }
 
-    private void startCurrentCoalTimer() {
+//        Starting the current coal countdown timer
+        mTimeLeftInMillis = coalFinishTime - calender.getTimeInMillis();
         mCurrentCoalTimer = new CountDownTimer(mCurrentCoalInMillis, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -139,83 +159,86 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
                 if (mTimerRunning) {
-                    restartCoalTimer();
-                } else {
-                    pauseCoalTimer();
+                    mCurrentCoalTimer.start();
                 }
             }
         }.start();
 
-        mCurrentCoalRunning = true;
     }
 
-    private void pauseTimer() {
-        mCountDownTimer.cancel();
-        mTimerRunning = false;
-        mButtonStartPause.setText("Start");
-        mButtonReset.setVisibility(View.VISIBLE);
-    }
-
-    private void pauseBurnTimer() {
-        mCurrentBurnTimer.cancel();
-        mCurrentBurnRunning = false;
-        mButtonAddCoal.setVisibility(View.INVISIBLE);
-    }
-
-    private void pauseCoalTimer() {
-        mCurrentCoalTimer.cancel();
-        mCurrentCoalRunning = false;
-    }
-
+    /**
+     * Method for resetting all three timers.
+     */
     private void resetTimer() {
-        coalAmount = 0;
         mTimeLeftInMillis = START_TIME_IN_MILLIS;
         mCurrentBurnInMillis = BURN_START_IN_MILLIS;
         mCurrentCoalInMillis = COAL_START_IN_MILLIS;
+
         updateCountDownText();
         updateCurrentBurnText();
         updateCurrentCoalText();
-        mButtonReset.setVisibility(View.INVISIBLE);
-        mButtonStartPause.setVisibility(View.VISIBLE);
+
+        mButtonStartReset.setVisibility(View.VISIBLE);
         mButtonAddCoal.setVisibility(View.INVISIBLE);
-        mCurrentBurnRunning = false;
-        mCurrentCoalRunning = false;
 
+        mTimerRunning = false;
     }
 
-    private void restartCoalTimer() {
-        startCurrentCoalTimer();
-    }
 
+    /**
+     * Method handling the Final Countdown Timer
+     */
     private void updateCountDownText() {
         int days = (int) (mTimeLeftInMillis / 1000) / 86400;
         int hours = (int) (mTimeLeftInMillis / 1000) / 3600 % 24;
         int minutes = (int) (mTimeLeftInMillis / 1000) / 60 % 60;
         int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
 
+        //Setting current burn time
         String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d:%02d:%02d", days, hours, minutes, seconds);
-
         mTextViewCountDown.setText(timeLeftFormatted);
     }
 
+    /**
+     * Method handling the Current Burn Countdown Timer
+     */
     private void updateCurrentBurnText() {
         int hours = (int) ((mCurrentBurnInMillis + (coalAmount * COAL_START_IN_MILLIS)) / 1000) / 3600 % 24;
         int minutes = (int) ((mCurrentBurnInMillis + (coalAmount * COAL_START_IN_MILLIS)) / 1000) / 60 % 60;
         int seconds = (int) ((mCurrentBurnInMillis + (coalAmount * COAL_START_IN_MILLIS)) / 1000) % 60;
 
+        //Setting coal burn time
         String currentBurnLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds);
-
         mTextViewCountDownTwo.setText(currentBurnLeftFormatted);
     }
 
+    /**
+     * Method handling the Current Coal Countdown Timer
+     */
     private void updateCurrentCoalText() {
-        int hours = (int) (mCurrentCoalInMillis  / 1000) / 3600 % 24;
+        int hours = (int) (mCurrentCoalInMillis / 1000) / 3600 % 24;
         int minutes = (int) (mCurrentCoalInMillis / 1000) / 60 % 60;
         int seconds = (int) (mCurrentCoalInMillis / 1000) % 60;
 
         String currentBurnCoalFormatted = String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds);
 
         mTextViewCountDownThree.setText(currentBurnCoalFormatted);
+    }
+
+    /**
+     * onSaveInstanceState override to save important values.
+     * ~needs more concrete data saving, so to make sure it's never lost until deletion.
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+
+        //Saving important values
+        outState.putLong(FINISH_TIME, finishTime);
+
+        outState.putBoolean(FINAL_TIMER_RUNNING, mTimerRunning);
+
+        // call superclass to save any view hierarchy
+        super.onSaveInstanceState(outState);
     }
 
 }
